050560ed7639d35b1bda3e46bffb31bfd6b12441
sergeiklochkov <43850350+sergeiklochkov@users.noreply.github.com>
2019-09-30 05:54:59 +0300

    Update table.py

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,8 +1,23 @@
 import sys
-
-# Ваши импорты
+import reader as a
+import writer as c
+import parсer as b
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-
-    # Ваш код
+    ind = True
+    try:
+        l = a.reader(filename)
+    except FileNotFoundError:
+        print("Файл не валиден")
+        ind = False
+    if l is None and ind:
+        print("Формат не валиден")
+    else:
+        if ind:
+            ll = b.parcer(l)
+    if ll is None and ind:
+        print("Формат не валиден")
+    else:
+        if ind:
+            c.writer(ll)
